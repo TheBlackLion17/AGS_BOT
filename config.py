b0b5f9572427117df67d02954f01fffff6e8b510
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2024-04-28 00:31:08 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config(object):
+    # pyro client config
+    API_ID    = os.environ.get("API_ID", "29812636")
+    API_HASH  = os.environ.get("API_HASH", "581c6dd6f0af0f8c8326c9b28920ae54")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6677982267:AAEcndDulVI6f9y4k_xC4UAVnY4vEPGzVU8") 
+
+    START_PIC   = os.environ.get("START_PIC", "https://telegra.ph/file/240720bec6145bb269f17.jpg")
