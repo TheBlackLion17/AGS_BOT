00394a9f63ce22769bd9ea9cfd64e2c2d68d6ff7
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2024-04-28 00:45:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,13 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6677982267:AAEcndDulVI6f9y4k_xC4UAVnY4vEPGzVU8") 
 
     START_PIC   = os.environ.get("START_PIC", "https://telegra.ph/file/240720bec6145bb269f17.jpg")
+
+class Txt(object):
+    # part of text configuration
+    START_TXT = """Halo {}  what's up
+
+𝘐 𝘢𝘮 𝘢 𝘙𝘦𝘯𝘢𝘮𝘦 𝘉𝘰𝘵 
+𝘠𝘰𝘶 𝘤𝘢𝘯 𝘶𝘴𝘦 𝘮𝘦 𝘧𝘰𝘳 𝘙𝘦𝘯𝘢𝘮𝘦 & 𝘊𝘩𝘢𝘯𝘨𝘦 𝘛𝘩𝘶𝘮𝘣𝘯𝘢𝘪𝘭 𝘖𝘧 𝘠𝘰𝘶𝘳 𝘍𝘪𝘭𝘦
+𝘐 𝘤𝘢𝘯 𝘢𝘭𝘴𝘰 𝘊𝘰𝘯𝘷𝘦𝘳𝘵 𝘝𝘪𝘥𝘦𝘰 𝘛𝘰 𝘍𝘪𝘭𝘦 & 𝘍𝘪𝘭𝘦 𝘛𝘰 𝘝𝘪𝘥𝘦𝘰 
+
+𝘐 𝘸𝘢𝘴 𝘔𝘢𝘥𝘦 𝘉𝘺 : @AgsModsOG"""
