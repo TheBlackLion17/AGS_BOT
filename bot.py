5c604a397ddd8ca074f5dc561a0d4a8955d4fca2
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2024-04-28 00:43:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,27 +4,36 @@ from pyrogram import Client, filters
 from commands.approvi import new_member_join
 from commands.start import start_command
 from commands.log import log_message
-
+from config import Config
+from pytz import timezone
 # Replace these placeholders with your actual values
-API_ID = "29812636"
-API_HASH = "581c6dd6f0af0f8c8326c9b28920ae54"
-BOT_TOKEN = "6677982267:AAEcndDulVI6f9y4k_xC4UAVnY4vEPGzVU8"
-LOG_CHANNEL_ID = -1002016756529  # Replace with your log channel ID
-START_PICTURE_URL = "https://telegra.ph/file/240720bec6145bb269f17.jpg"  # Replace with your start picture URL
+
+LOG_CHANNEL = -1002016756529  # Replace with your log channel ID
 
 # Initialize the Pyrogram Client
-app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-
-# Register the handler for new member join events
-app.on_message(filters.new_chat_members)(new_member_join)
-
-@app.on_message(filters.command("start"))
-def start_command(client, message):
-    if START_PICTURE_URL:
-        client.send_photo(
-            chat_id=message.chat.id,
-            photo=START_PICTURE_URL,
-            caption="Welcome to the group! Feel free to introduce yourself."
+class app(Client):
+    def __init__(self):
+        super().__init__(
+            api_id=Config.API_ID,
+            api_hash=Config.API_HASH,
+            bot_token=Config.BOT_TOKEN
         )
+
+    async def start(self):
+        await super().start()
+        me = await self.get_me()
+        self.mention = me.mention
+        self.username = me.username
+
+   if LOG_CHANNEL:
+       try:
+                curr = datetime.now(timezone("Asia/Kolkata"))
+                date = curr.strftime('%d %B, %Y')
+                time = curr.strftime('%I:%M:%S %p')
+                await self.send_message(Config.LOG_CHANNEL, f"**{me.mention} Is Restarted !!**\n\n📅 Date : `{date}`\n⏰ Time : `{time}`\n🌐 Timezone : `Asia/Kolkata`\n\n🉐 Version : `v{__version__} (Layer {layer})`</b>")                                
+            except:
+                print("Please Make This Is Admin In Your Log Channel")
+
+
 # Start the Pyrogram Client
 app.run()
