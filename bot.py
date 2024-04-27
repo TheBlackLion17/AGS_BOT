f7183388db472a07ec2cfe1327740f775b59fcaa
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2024-04-28 00:09:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,16 @@
 # bot.py
 
 from pyrogram import Client, filters
-from commands.new_member_handler import new_member_join
-from commands.start_command import start_command
-from commands.log_message import log_message
+from commands.approvi import new_member_join
+from commands.start import start_command
+from commands.log import log_message
 
 # Replace these placeholders with your actual values
-API_ID = "YOUR_API_ID"
-API_HASH = "YOUR_API_HASH"
-BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
-LOG_CHANNEL_ID = -100123456789  # Replace with your log channel ID
-START_PICTURE_URL = "URL_TO_YOUR_START_PICTURE"  # Replace with your start picture URL
+API_ID = "29812636"
+API_HASH = "581c6dd6f0af0f8c8326c9b28920ae54"
+BOT_TOKEN = "6677982267:AAEcndDulVI6f9y4k_xC4UAVnY4vEPGzVU8"
+LOG_CHANNEL_ID = -1002016756529  # Replace with your log channel ID
+START_PICTURE_URL = "https://telegra.ph/file/240720bec6145bb269f17.jpg"  # Replace with your start picture URL
 
 # Initialize the Pyrogram Client
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
