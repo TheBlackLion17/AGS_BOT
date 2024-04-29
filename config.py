37a130977bcd14d1292641ba794842c83cfaf7cc
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2024-04-30 02:11:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "29812636")
-    API_HASH  = os.environ.get("API_HASH", "581c6dd6f0af0f8c8326c9b28920ae54")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6677982267:AAEcndDulVI6f9y4k_xC4UAVnY4vEPGzVU8") 
+    API_ID    = "29812636"
+    API_HASH  = "581c6dd6f0af0f8c8326c9b28920ae54")
+    BOT_TOKEN = "6677982267:AAEcndDulVI6f9y4k_xC4UAVnY4vEPGzVU8"
+    ADMIN_USER_ID = "5909932224"
 
     START_PIC   = os.environ.get("START_PIC", "https://telegra.ph/file/240720bec6145bb269f17.jpg")
 
