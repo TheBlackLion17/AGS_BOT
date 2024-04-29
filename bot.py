f4a94433627c58aad2373a843bf0c7569f192e77
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2024-04-30 02:55:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,55 +7,65 @@ from config import API_ID, API_HASH, BOT_TOKEN
 # Initialize the Pyrogram Client
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
+# Dictionary to store the state of the conversation for each user
+conversation_state = {}
+
 # Handler for the /start command
 @app.on_message(filters.command("start"))
 async def start_command(client, message: Message):
+    # Set the state of the conversation to "awaiting_image_and_caption"
+    conversation_state[message.chat.id] = "awaiting_image_and_caption"
+    
     # Prompt the user to set the image and caption
     await message.reply_text("Please set the image and caption.")
 
-    # Set the next handler to handle image and caption setting
-    await app.set_next_step(chat_id=message.chat.id, action=set_image_and_caption)
-
-# Handler to handle image and caption setting
-async def set_image_and_caption(client, message: Message):
-    # Check if the message contains an image
-    if message.photo:
-        # Get the photo
-        photo = message.photo[-1]
-        # Download the photo
-        photo_path = await photo.download(file_name="image.jpg")
-
-        # Set the image and caption
-        app.image_path = photo_path
-        app.caption = message.text
-
-        # Ask the user to send the file
-        await message.reply_text("Please send the file.")
-    else:
-        # If the message does not contain an image, ask the user to resend
-        await message.reply_text("Please send the image as a photo.")
-
-# Handler for file sent by the user
+# Handler for messages
 @app.on_message()
-async def handle_file(client, message: Message):
-    # Check if the bot has set the image and caption
-    if hasattr(app, 'image_path') and hasattr(app, 'caption'):
+async def handle_message(client, message: Message):
+    # Check if the user is in the "awaiting_image_and_caption" state
+    if message.chat.id in conversation_state and conversation_state[message.chat.id] == "awaiting_image_and_caption":
+        # Check if the message contains an image
+        if message.photo:
+            # Get the photo
+            photo = message.photo[-1]
+            # Download the photo
+            photo_path = await photo.download(file_name="image.jpg")
+
+            # Set the state of the conversation to "awaiting_file"
+            conversation_state[message.chat.id] = "awaiting_file"
+
+            # Set the image path and caption in the conversation state
+            conversation_state[message.chat.id + "_image_path"] = photo_path
+            conversation_state[message.chat.id + "_caption"] = message.text
+
+            # Prompt the user to send the file
+            await message.reply_text("Please send the file.")
+        else:
+            # If the message does not contain an image, ask the user to resend
+            await message.reply_text("Please send the image as a photo.")
+    # Check if the user is in the "awaiting_file" state
+    elif message.chat.id in conversation_state and conversation_state[message.chat.id] == "awaiting_file":
         # Check if the message contains a file
         if message.document:
             # Download the file
             file_path = await message.download(file_name="file")
 
             # Send the file with the set caption
-            await message.reply_photo(photo=app.image_path, caption=app.caption)
+            await message.reply_photo(
+                photo=conversation_state[message.chat.id + "_image_path"],
+                caption=conversation_state[message.chat.id + "_caption"]
+            )
 
             # Delete the message after sending the file
             await message.delete()
+
+            # Clear the conversation state
+            del conversation_state[message.chat.id]
+            del conversation_state[message.chat.id + "_image_path"]
+            del conversation_state[message.chat.id + "_caption"]
         else:
             # If the message does not contain a file, ask the user to resend
             await message.reply_text("Please send the file.")
-    else:
-        # If the bot has not set the image and caption, ignore the file
-        pass
 
 # Start the Pyrogram Client
 app.run()
