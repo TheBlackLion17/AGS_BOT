8066ce1cbe2a4ec337933386e3824d009de92ee5
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2024-05-28 19:56:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,11 @@
 import os, math, logging, datetime, pytz
-import logging.config
 
 from pyrogram.errors import BadRequest, Unauthorized
 from pyrogram import Client
 from pyrogram import types
 
-from database.ia_filterdb import Media
-from database.users_chats_db import db
+
 from info import API_ID, API_HASH, BOT_TOKEN, LOG_CHANNEL, UPTIME, WEBHOOK, LOG_MSG
-from utils import temp, __repo__, __license__, __copyright__, __version__
-from typing import Union, Optional, AsyncGenerator
 
 from plugins import web_server 
 from aiohttp import web
