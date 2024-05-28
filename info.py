8b64166b91bc2014bf69a9634e6219e7ab48d2f3
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2024-05-28 19:50:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,9 +12,9 @@ def is_enabled(value, default):
 
 API_ID = int(environ('API_ID','20919286'))
 API_HASH = environ('API_HASH','57b85f72104db3f08f9795b0410eb556')
-BOT_TOKEN = environ('BOT_TOKEN','6900955318:AAFW2S6dhijU5ybVSH9qoPi-xucjBqDTqzE')
+BOT_TOKEN = environ('BOT_TOKEN','7101381082:AAFOaYNVIksYKcRyEzbZ5Aa8Xrh4UwnE-K8')
 
 WEBHOOK = bool(environ.get("WEBHOOK", True)) # for web support on/off
 PICS = (environ.get('PICS' ,'https://telegra.ph/file/240720bec6145bb269f17.jpg')).split()
 UPTIME = time.time()
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', "-1002038658435"))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', "-1002128069308"))
