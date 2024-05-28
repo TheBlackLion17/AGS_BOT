68d39ac0624cf4dd75b097b0f9d0b976161acbca
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2024-05-28 19:32:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -10,9 +10,9 @@ def is_enabled(value, default):
     else: return default
 
 
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+API_ID = int(environ('API_ID','20919286'))
+API_HASH = environ('API_HASH','57b85f72104db3f08f9795b0410eb556')
+BOT_TOKEN = environ('BOT_TOKEN','6900955318:AAFW2S6dhijU5ybVSH9qoPi-xucjBqDTqzE')
 
 WEBHOOK = bool(environ.get("WEBHOOK", True)) # for web support on/off
 PICS = (environ.get('PICS' ,'https://graph.org/file/01ddfcb1e8203879a63d7.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg https://graph.org/file/a125497b6b85a1d774394.jpg https://graph.org/file/43d26c54d37f4afb830f7.jpg https://graph.org/file/60c1adffc7cc2015f771c.jpg https://graph.org/file/d7b520240b00b7f083a24.jpg https://graph.org/file/0f336b0402db3f2a20037.jpg https://graph.org/file/39cc4e15cad4519d8e932.jpg https://graph.org/file/d59a1108b1ed1c6c6c144.jpg https://te.legra.ph/file/3a4a79f8d5955e64cbb8e.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg')).split()
