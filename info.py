1738d7e72551e549da2d83826a3b9a6ff528a77a
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2024-05-28 20:03:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -10,8 +10,8 @@ def is_enabled(value, default):
     else: return default
 
 
-API_ID = int(environ('API_ID','20919286'))
-API_HASH = environ('API_HASH','57b85f72104db3f08f9795b0410eb556')
+API_ID = int(environ('API_ID','6976445947'))
+API_HASH = environ('API_HASH','d39c4324a40a8a6b27a067f8ff2b987e')
 BOT_TOKEN = environ('BOT_TOKEN','7101381082:AAFOaYNVIksYKcRyEzbZ5Aa8Xrh4UwnE-K8')
 
 WEBHOOK = bool(environ.get("WEBHOOK", True)) # for web support on/off
