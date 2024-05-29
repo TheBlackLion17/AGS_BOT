19f29b5ade38a8bc880e65bb7699d1dbce2aa84f
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2024-05-29 19:17:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            name="renamer",
+            name="agsbot",
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
             bot_token=Config.BOT_TOKEN,
