0d8355c809632c0f7272cf6915760711bab8ee2a
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2024-05-29 19:14:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -9,15 +9,14 @@ def is_enabled(value, default):
     elif value.strip().lower() in ["off", "false", "no", "0", "disable", "n"]: return False
     else: return default
 
-
-# PyroClient Setup 
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
-
-WEBHOOK = bool(environ.get("WEBHOOK", True)) # for web support on/off
-PICS = (environ.get('PICS' ,'https://telegra.ph/file/240720bec6145bb269f17.jpg')).split()
-UPTIME = time.time()
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', "-1002128069308"))
-
-LOG_MSG = "{} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ....✨\n\n🗓️ Dᴀᴛᴇ : {}\n⏰ Tɪᴍᴇ : {}\n\n🖥️ Rᴇᴏᴩ: {}\n🉐 Vᴇʀsɪᴏɴ: {}\n🧾 Lɪᴄᴇɴꜱᴇ: {}\n©️ Cᴏᴩʏʀɪɢʜᴛ: {}"
+class Config(object):
+    API_ID = int(environ['API_ID'])
+    API_HASH = environ['API_HASH']
+    BOT_TOKEN = environ['BOT_TOKEN']
+    
+    WEBHOOK = bool(environ.get("WEBHOOK", True)) # for web support on/off
+    PICS = (environ.get('PICS' ,'https://telegra.ph/file/240720bec6145bb269f17.jpg')).split()
+    UPTIME = time.time()
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', "-1002128069308"))
+    
+    LOG_MSG = "{} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ....✨\n\n🗓️ Dᴀᴛᴇ : {}\n⏰ Tɪᴍᴇ : {}\n\n🖥️ Rᴇᴏᴩ: {}\n🉐 Vᴇʀsɪᴏɴ: {}\n🧾 Lɪᴄᴇɴꜱᴇ: {}\n©️ Cᴏᴩʏʀɪɢʜᴛ: {}"
